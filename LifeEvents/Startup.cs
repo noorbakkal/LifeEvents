88c1d0c7e835e062d61ef5301c19702858a4e6d4
Jack Tamas <lovely_gad@yahoo.com>
2018-11-24 23:11:06 -0800

    I added Comment

--- LifeEvents/Startup.cs
+++ LifeEvents/Startup.cs
@@ -17,7 +17,7 @@ namespace LifeEvents
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. This IS JAck
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
